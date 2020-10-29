44a6881ef3a12846cae11ebe7e7ad6fbef033d8d
AmandaLW <mrs.amandalynnward@gmail.com>
2020-10-28 20:54:34 -0700

    changed the about page text

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -56,7 +56,7 @@ export default class About extends Component {
                     <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
+                    <h3>Robotics</h3>
                     <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
                 </div>
                 </div>
