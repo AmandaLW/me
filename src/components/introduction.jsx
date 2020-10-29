068e7574292281e277a0fba92a03f2f0386aee09
AmandaLW <mrs.amandalynnward@gmail.com>
2020-10-28 23:05:26 -0700

    added link to my resume

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1>Hi! <br />I'm Amanda.</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/1e2f1_Nc1lCttohXmU56YMFV4-I2buWLyJ2_GLEpQ_74/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
